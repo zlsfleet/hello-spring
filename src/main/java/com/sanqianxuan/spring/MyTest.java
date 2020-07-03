b87ebfc6dc1b3bc25fad4479c0eccfe556e75b30
zlsfleet <kircheiszls@gmail.com>
2020-07-03 15:40:47 +0800

    update

--- src/main/java/com/sanqianxuan/spring/MyTest.java
+++ src/main/java/com/sanqianxuan/spring/MyTest.java
@@ -1,7 +1,8 @@
 package com.sanqianxuan.spring;
 
 import com.sanqianxuan.spring.service.UserService;
-import com.sanqianxuan.spring.service.impl.UserServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,9 +12,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @since 2020/6/23 15:12
  */
 public class MyTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyTest.class);
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.sayHi();
+
+        logger.info("info level");
+        logger.debug("debug level");
+        logger.error("error level");
+        logger.trace("trace level");
+        logger.warn("warn level");
+
+        String message = "test";
+
     }
 }
